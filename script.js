26babc536ec0ec5dec9c615dc13e580db30978a1
Vistor0808 <chwamanikho@gmail.com>
2025-11-09 07:24:08 +0000

    with javascript

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Hello Bafo")
+"use strict"
+let message;
+
+let x = 12
+let y = 7
+message = x + y + 1
+let b = "Lizo"
+alert(typeof message)
+alert(typeof b)
+let age = prompt("How old re you?",)
+alert(`You are ${age} years old!`)
+ let isBoss = confirm("Are you the boss?",)
+ alert(isBoss)
+ 
